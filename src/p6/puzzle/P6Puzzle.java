cc95fe61c275ff6ee23688d45aeb1a75d369825a
apayeras <apayerasmunar@gmail.com>
2022-05-23 15:06:44 +0200

    p6

---
+++ src/p6/puzzle/P6Puzzle.java
@@ -0,0 +1,15 @@
+package p6.puzzle;
+
+/**
+ * @author Antoni
+ */
+public class P6Puzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
